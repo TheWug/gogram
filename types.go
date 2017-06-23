75564757b8c662c1bb5625a203523c3ccfce267d
Derp Derpingston <rjs3275@g.rit.edu>
2017-06-23 02:27:29 -0700

    Add support for a number of additional message types as well as edited messages

--- types.go
+++ types.go
@@ -5,14 +5,14 @@ import (
 )
 
 type TUser struct {
-	Id         int    `json:"id"`
-	First_name string `json:"first_name"`
-	Last_name  string `json:"last_name"`
-	Username   string `json:"username"`
+	Id          int    `json:"id"`
+	First_name  string `json:"first_name"`
+	Last_name  *string `json:"last_name"`
+	Username   *string `json:"username"`
 }
 
 type TChat struct {
-	Id          int64    `json:"id"`
+	Id          int64  `json:"id"`
 	Type        string `json:"type"`
 	Title      *string `json:"title"`
 	Username   *string `json:"username"`
@@ -38,27 +38,72 @@ type TMessageEntity struct {
 }
 
 type TMessage struct {
-	Message_id         int              `json:"message_id"`
-    From              *TUser            `json:"from"`
-	Date               int              `json:"date"`
-	Chat               TChat            `json:"chat"`
-	Forward_from      *TUser            `json:"forward_from"`
-	Forward_from_chat *TChat            `json:"forward_from_chat"`
-	Forward_date      *int              `json:"forward_date"`
-	Reply_to_message  *TMessage         `json:"reply_to_message"`
-	Edit_date         *int              `json:"edit_date"`
-	Text              *string           `json:"text"`
-	Sticker           *TSticker         `json:"sticker"`
-	Caption           *string           `json:"caption"`
-	Entities          *[]TMessageEntity `json:"entities"`
-//	Audio             *TAudio    `json:"audio"`
-//	Document          *TDocument `json:"document"`
-//	Photo             *TPhoto    `json:"photo"`
-//	Video             *TVideo    `json:"video"`
-//	Voice             *TVoice    `json:"voice"`
-//	Contact           *TContact  `json:"contact"`
-//	Location          *TLocation `json:"location"`
-//	Venue             *TVenue    `json:"venue"`
+	Message_id          int              `json:"message_id"`
+    From               *TUser            `json:"from"`
+	Date                int              `json:"date"`
+	Chat                TChat            `json:"chat"`
+	Forward_from       *TUser            `json:"forward_from"`
+	Forward_from_chat  *TChat            `json:"forward_from_chat"`
+	Forward_date       *int              `json:"forward_date"`
+	Reply_to_message   *TMessage         `json:"reply_to_message"`
+	Edit_date          *int              `json:"edit_date"`
+	Text               *string           `json:"text"`
+	Caption            *string           `json:"caption"`
+	Entities           *[]TMessageEntity `json:"entities"`
+	Audio              *TGenericFile     `json:"audio"`
+	Document           *TGenericFile     `json:"document"`
+	Game               *TGame            `json:"game"`
+	Photo              *[]TPhotoSize     `json:"photo"`
+	Sticker            *TSticker         `json:"sticker"`
+	Video              *TGenericFile     `json:"video"`
+	Voice              *TGenericFile     `json:"voice"`
+	Contact            *TContact         `json:"contact"`
+	Location           *TLocation        `json:"location"`
+	Venue              *TVenue           `json:"venue"`
+    New_chat_member    *TUser            `json:"new_chat_member"`
+    Left_chat_member   *TUser            `json:"left_chat_member"`
+    New_chat_title     *string           `json:"new_chat_title"`
+    New_chat_photo     *[]TPhotoSize     `json:"new_chat_photo"`
+	Delete_chat_photo  *bool             `json:"delete_chat_photo"`
+	Group_chat_created *bool             `json:"group_chat_created"`
+	Supergroup_chat_created *bool        `json:"supergroup_chat_created"`
+	Channel_chat_created    *bool        `json:"channel_chat_created"`
+	Migrate_to_chat_id      *int64       `json:"migrate_to_chat_id"`
+	Migrate_from_chat_id    *int64       `json:"migrate_from_chat_id"`
+	Pinned_message     *TMessage         `json:"pinned_message"`
+}
+
+type TGenericFile struct {
+	File_id string `json:"file_id"`
+}
+
+type TGame struct {
+	Title string `json:"title"`
+}
+
+type TPhotoSize struct {
+	File_id   string `json:"file_id"`
+	Width     int    `json:"width"`
+	Height    int    `json:"height"`
+	File_size int    `json:"file_size"`
+}
+
+type TContact struct {
+	Phone_number string `json:"phone_number"`
+	First_name   string `json:"first_name"`
+	Last_name    string `json:"last_name"`
+	User_id      int    `json:"user_id"`
+}
+
+type TLocation struct {
+	Longitude string `json:"longitude"`
+	Latitude  string `json:"latitude"`
+}
+
+type TVenue struct {
+	Location    TLocation `json:"location"`
+	Title       string    `json:"title"`
+	Description string    `json:"description"`
 }
 
 type TInlineQuery struct {
@@ -79,6 +124,7 @@ type TChosenInlineResult struct {
 type TUpdate struct {
 	Update_id             int                 `json:"update_id"`
 	Message              *TMessage            `json:"message,omitempty"`
+	Edited_message       *TMessage            `json:"edited_message,omitempty"`
 	Inline_query         *TInlineQuery        `json:"inline_query,omitempty"`
 	Chosen_inline_result *TChosenInlineResult `json:"chosen_inline_result,omitempty"`
 //	Callback_query       *TCallbackQuery      `json:"callback_query,omitempty"`
