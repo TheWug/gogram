31ba3726608c658308e93194a7ae36a47c2b0340
Derp Derpingston <rjs3275@g.rit.edu>
2019-06-03 02:57:57 -0700

    Describe NormalizeDashes, since it's not used anywhere within the library

--- replace.go
+++ replace.go
@@ -4,6 +4,9 @@ import (
 	"strings"
 )
 
+// A helper function which replaces em-dashes with double dashes.
+// this reverses a substition some telegram clients perform automatically
+// and should be used when performing argparse-like command processing.
 func NormalizeDashes(cmd string) (string) {
 	return strings.Replace(cmd, "—", "--", -1)
 }
