de3e3c8c4969a0a31bf0cfaecb1e719135e481c4
Derp Derpingston <rjs3275@g.rit.edu>
2019-06-02 21:19:15 -0700

    Add outgoing data class location

---
+++ out_types.go
@@ -0,0 +1,14 @@
+package gogram
+
+import (
+)
+
+type OMessage struct {
+	ChatID              interface{}
+	Text                string
+	ParseMode           string
+	EnableWebPreview    bool	// I decided to flip this one because in general, disabling it is a more sensible default.
+	DisableNotification bool
+	ReplyTo            *int
+	ReplyMarkup         interface{}
+}
