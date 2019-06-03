159afc68cca975281c323edd89424bd0a0e46896
Derp Derpingston <rjs3275@g.rit.edu>
2019-06-03 03:00:40 -0700

    Improve NormalizeDashes to only operate on leading em dashes

--- replace.go
+++ replace.go
@@ -4,9 +4,16 @@ import (
 	"strings"
 )
 
-// A helper function which replaces em-dashes with double dashes.
+// A helper function which replaces leading em-dashes with double dashes.
 // this reverses a substition some telegram clients perform automatically
 // and should be used when performing argparse-like command processing.
 func NormalizeDashes(cmd string) (string) {
-	return strings.Replace(cmd, "—", "--", -1)
+	new_cmd := strings.TrimPrefix(cmd, "—") // remove em dash
+	if cmd != new_cmd {
+		// if the original string WAS prefixed with an em dash, add a double dash instead.
+		return "--" + new_cmd
+	} else {
+		// otherwise, return the original string unchanged.
+		return cmd
+	}
 }
