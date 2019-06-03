1effe2b3f64368ddf931245a0874dbf4eb3f7e7a
Derp Derpingston <rjs3275@g.rit.edu>
2019-06-03 04:18:10 -0700

    Enable both sending and editing messages with data.OMessage

--- data/out_types.go
+++ data/out_types.go
@@ -7,11 +7,18 @@ const HTML string = "HTML"
 const Markdown string = "Markdown"
 
 type OMessage struct {
-	ChatID              interface{}
+	// used for all outgoing messages
+	ChatID              interface{} // not used for editing messages if InlineID is present
 	Text                string
 	ParseMode           string
 	EnableWebPreview    bool	// I decided to flip this one because in general, disabling it is a more sensible default.
+	ReplyMarkup         interface{}
+
+	// only used for new messages
 	DisableNotification bool
 	ReplyTo            *int
-	ReplyMarkup         interface{}
+
+	// only used for message edits
+	MessageID           int
+	InlineID            string
 }
