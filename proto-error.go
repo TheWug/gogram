ba82a47ca677bc24b8322effb112fb629a4ca2da
Derp Derpingston <rjs3275@g.rit.edu>
2019-10-11 02:02:47 -0700

    Use custom error class, allowing more finely tuned responses to endpoint status

--- proto-error.go
+++ proto-error.go
@@ -3,13 +3,28 @@ package gogram
 import (
 	"github.com/thewug/gogram/data"
 
-	"errors"
 	"fmt"
 )
 
+type ProtocolError struct {
+	HTTPCode    int
+	Description string
+}
+
+func (this ProtocolError) Error() (string) {
+	return fmt.Sprintf("Failure indicated by API endpoint (%d: %s)\n", this.HTTPCode, this.Description)
+}
+
+func NewError(code int, description string) (*ProtocolError) {
+	return &ProtocolError{
+		HTTPCode: code,
+		Description: description,
+	}
+}
+
 func HandleSoftError(resp *data.TGenericResponse) (error) {
 	if resp.Ok != true {
-		return errors.New(fmt.Sprintf("Failure indicated by API endpoint (%d: %s)\n", *resp.Error_code, *resp.Description))
+		return NewError(*resp.Error_code, *resp.Description)
 	}
 	return nil
 }
