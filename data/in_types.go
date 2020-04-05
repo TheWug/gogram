c6918cb5ce03ea5db700870e2086a621b59ed44e
Derp Derpingston <rjs3275@g.rit.edu>
2020-04-05 12:52:07 -0700

    Add TAnimation, incorporated in bot api 4.0

--- data/in_types.go
+++ data/in_types.go
@@ -164,6 +164,18 @@ type TDocument struct {
 	File_size  int        `json:"file_size"`
 }
 
+type TAnimation struct {
+	File_id    string     `json:"file_id"`
+	Unique_id  string     `json:"file_unique_id"`
+	Width      int        `json:"width"`
+	Height     int        `json:"height"`
+	Duration   int        `json:"duration"`
+	Thumb     *TPhotoSize `json:"thumb"`
+	File_name *string     `json:"file_name"`
+	Mime_type *string     `json:"mime_type"`
+	File_size *int        `json:"file_size"`
+}
+
 type TGame struct {
 	Title string `json:"title"`
 }
